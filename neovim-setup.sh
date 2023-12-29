007601ae76889ff93733cc3c7866d4ca554d470c
henkhank004 <90721658+20463702@users.noreply.github.com>
2023-12-29 18:26:30 +0100

    v1.0.1

--- neovim-setup.sh
+++ neovim-setup.sh
@@ -6,7 +6,8 @@ echo "alias vim=\"nvim\"" >> ~/.bashrc
 git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
 git clone https://github.com/20463702/neovim-setup/ $XDG_CONFIG_HOME/
-mv $XDG_CONFIG_HOME/neovim-setup $XDG_CONFIG_HOME/nvim
+mv $XDG_CONFIG_HOME/neovim-setup/* $XDG_CONFIG_HOME/nvim
+rmdir $XDG_CONFIG_HOME/neovim-setup/
 
 cd $XDG_CONFIG_HOME/nvim
 echo "Open neovim and run \":PackerSync\""
